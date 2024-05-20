973c41fe6cb46da0585a2dcfa56d1f06ffe73201
ibanezfr <francoibanez.dev@gmail.com>
2024-05-20 16:35:47 -0300

    add ejemplo-01-A.c

---
+++ clase-06/ejemplo-01-A.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+   float Total;
+   float Nota;
+   float Prom;
+   int i;
+
+   Total = 0.0;
+   Nota = 0.0;
+   Prom = 0.0;
+
+   for (i = 1; i <= 40; i++) {
+      printf("Ingrese la nota del alumno %d > ", i);
+      scanf("%f", &Nota);
+      Total += Nota;
+   }
+
+   Prom = (Total / 40 * 100) / 100;
+
+   printf("Promedio: %.2f\n", Prom);
+
+   return 0;
+}
